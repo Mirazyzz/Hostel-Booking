5837f186a0fbf15d356f2626dc2cb5f832860416
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-01-11 21:57:51 +0100

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
+
 import Home from './pages/Home';
 import SingleRoom from './pages/SingleRoom';
 import Rooms from './pages/Rooms';
 import Error from './pages/Error';
-import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/rooms" component={Rooms} />
-      <Route exact path="/rooms/:slug" component={SingleRoom} />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route exact component={Error} />
+      </Switch>
     </>
   );
 }
-
-//<Route exact component={Error} />
 
 export default App;
